e2a8d7c605d1cb663d37e859e7ead9dd4514e6dc
hira480 <hiramannan8@gmail.com>
2022-04-11 14:51:31 +0600

    added onauthstate change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import useFirebase from '../../hooks/useFirebase';
 import './Header.css';
 
 const Header = () => {
-    const { user } = useFirebase();
+    const { user, handelSignOut } = useFirebase();
     return (
         <div className='header'>
             <nav>
@@ -12,10 +12,11 @@ const Header = () => {
                 <Link to='/products'>Products</Link>
                 <Link to='/orders'>Orders</Link>
                 <Link to='/register'>Register</Link>
+                <span>{user?.displayName && user.displayName}</span>
                 {
-                    user.uid
+                    user?.uid
                         ?
-                        <button>Sign Out</button>
+                        <button onClick={handelSignOut}>Sign Out</button>
                         :
                         <Link to='/login'>Login</Link>
                 }
